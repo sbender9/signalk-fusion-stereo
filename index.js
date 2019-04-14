0cef8729dd169a06844717d9db46aa956678a436
Scott Bender <scott@scottbender.net>
2019-04-14 10:54:39 -0400

    break n2k code into separate file fix crash with invalid source info

--- index.js
+++ index.js
@@ -19,26 +19,7 @@ const child_process = require('child_process')
 const path = require('path')
 const os = require('os')
 const util = require('util')
-
-
-const fusion_commands = {
-  "next": "%s,6,126720,%s,%s,6,a3,99,03,00,%s,04",
-  "prev": "%s,6,126720,%s,%s,6,a3,99,03,00,%s,06",
-  "SiriusXM_next": "%s,7,126720,%s,%s,8,a3,99,1e,00,%s,01,00,00",
-  "SiriusXM_prev": "%s,7,126720,%s,%s,8,a3,99,1e,00,%s,02,00,00",
-  "play": "%s,6,126720,%s,%s,6,a3,99,03,00,%s,01",
-  "pause": "%s,6,126720,%s,%s,6,a3,99,03,00,%s,02",
-  "status": "%s,6,126720,%s,%s,4,a3,99,01,00",
-  "mute": "%s,6,126720,%s,%s,5,a3,99,11,00,01",
-  "unmute": "%s,6,126720,2,10,5,a3,99,11,00,02",
-  "setSource": "%s,6,126720,%s,%s,5,a3,99,02,00,%s",
-  "setVolume": "%s,6,126720,%s,%s,6,a3,99,18,00,%s,%s",
-  "setAllVolume": "%s,6,126720,%s,%s,8,a3,99,19,00,%s,%s,%s,%s",
-  "poweron": "%s,6,126720,%s,%s,5,a3,99,1c,00,01"
-}
-
-const default_src = '1'
-const everyone_dst = '255'
+const { getN2KCommand, zoneIdToNum, sourceidToNum } = require('./n2k_commands')
 
 const default_device = 'entertainment.device.fusion1'
 
@@ -218,7 +199,7 @@ module.exports = function(app) {
     
     for ( var key in sources )
     {
-      if ( sources[key].name.value == input )
+      if ( key && sources[key].name && sources[key].name.value == input )
         return key
     }
 
@@ -351,34 +332,48 @@ module.exports = function(app) {
   return plugin;
 }
 
-function padd(n, p, c)
-{
-  var pad_count = typeof p !== 'undefined' ? p : 2
-  var pad_char = typeof c !== 'undefined' ? c : '0';
-  var pad = new Array(1 + pad_count).join(pad_char);
-  return (pad + n).slice(-pad.length);
-}
-
-function isoDate()
-{
-  return (new Date()).toISOString()
-}
-
 function checkVolume(val)
 {
   return typeof val !== 'undefined' ? val : 0;
 }
 
-function zoneIdToNum(id)
+function sendCommand(app, deviceid, command_json)
 {
-  return padd((Number(id.substring("zone".length))-1).toString(16))
+  var n2k_msg = null
+  var action = command_json["action"]
+  
+  app.debug("command: %j", command_json)
+
+  let currentSource
+  let cur_source_id
+  
+  if ( action == 'next' || action == 'prev' || action == 'play'
+       || action == 'pause' )
+  {
+    cur_source_id = app.getSelfPath(
+	  device + ".output.zone1.source.value")
+
+    cur_source_id = cur_source_id.substring((device + '.avsource.').length)
+    var sources = app.getSelfPath(device + ".avsource")
+    app.debug("sources: %j cur_source_id: %s", sources, cur_source_id)
+    if (typeof cur_source_id != "undefined" && typeof sources != "undefined")
+    {
+      currentSource = sources[cur_source_id]["name"]["value"]
+    }
+  }
+
+  n2k_msg = getN2KCommand(deviceid, command_json, currentSource, cur_source_id)
+
+  if ( n2k_msg )
+  {
+    app.debug("n2k_msg: " + n2k_msg)
+    app.emit('nmea2000out', n2k_msg);
+  }
 }
 
-function sourceidToNum(id)
-{
-  return padd(Number(id.substring("source".length)).toString(16))
-}
 
+
+/*
 function sendCommand(app, deviceid, command_json)
 {
   var n2k_msg = null
@@ -452,3 +447,4 @@ function sendCommand(app, deviceid, command_json)
   }
 }
 
+*/
